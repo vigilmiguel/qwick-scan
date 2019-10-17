e7e65117f8d2e5f01db38acc9e2dcedf5a782f81
eggoJJ <40075327+eggoJJ@users.noreply.github.com>
2019-10-17 16:10:17 -0700

    Most of the products Class/Functions

---
+++ database-api/php_rest_api/api/products/updateProduct.php
@@ -0,0 +1,38 @@
+<?php
+    ini_set('display_errors', 1);
+
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Methods: POST');
+    header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Access-Control-Allow-Methods, Content-Type, ' . 
+            'Authorization, X-Requested-With');
+
+    include_once '../../config/Database.php';
+    include_once '../../models/Products.php';
+
+    $database = new Database();
+    $db = $database->connect();
+    
+    $product = new Products($db);
+
+    $data = json_decode(file_get_contents("php://input"));
+
+    $product->productid = $data->productid;
+    $product->barcode = $data->barcode;
+    $product->productname = $data->productname;
+    $product->imageurl = $data->imageurl;
+
+    if($product->update())
+    {
+        echo json_encode(
+            array('message' => 'Updated')
+        );
+    }
+    else
+    {
+        echo json_encode(
+            array('message' => 'ERROR: User Not Created')
+        );
+    }
+
+?>
