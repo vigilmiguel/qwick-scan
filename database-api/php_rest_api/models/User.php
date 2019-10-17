be43bfd14d4adee196c221f081cb66fd92fe35ff
eggoJJ <40075327+eggoJJ@users.noreply.github.com>
2019-10-17 13:13:20 -0700

    Update Single User

--- database-api/php_rest_api/models/User.php
+++ database-api/php_rest_api/models/User.php
@@ -68,6 +68,40 @@
 
             return false;
         }
+
+        public function update()
+        {
+            #$query = "  INSERT INTO users   (firebaseUID, userName)
+                            //VALUES          (:firebaseUID, :userName) 
+                            //WHERE userid = :userid";
+
+            $query = "UPDATE users
+                SET
+                  firebaseUID = :firebaseUID,
+                  userName = :userName
+                  WHERE
+                    userid = :userid";
+
+            $stmt = $this->conn->prepare($query);
+
+            // Clean the data.
+            $this->firebaseUID = htmlspecialchars(strip_tags($this->firebaseUID));
+            $this->userName = htmlspecialchars(strip_tags($this->userName));
+            $this->userid = htmlspecialchars(strip_tags($this->userid));
+
+            $stmt->bindParam(':firebaseUID', $this->firebaseUID);
+            $stmt->bindParam(':userName', $this->userName);
+            $stmt->bindParam(':userid', $this->userid);
+
+            if($stmt->execute())
+                return true;
+            
+            printf("Error: %s.\n", $stmt->error);
+
+            return false;
+        }
+
+
     }
 
 
