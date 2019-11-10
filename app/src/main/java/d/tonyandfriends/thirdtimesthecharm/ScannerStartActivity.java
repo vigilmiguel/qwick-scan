a58fb4a887bce135a4609b1de06f7dcc28fa791e
Vishant <vishantk@gmail.com>
2019-11-10 12:13:33 -0800

    Added cleartext permissions, raw json parsable and settable

--- app/src/main/java/d/tonyandfriends/thirdtimesthecharm/ScannerStartActivity.java
+++ app/src/main/java/d/tonyandfriends/thirdtimesthecharm/ScannerStartActivity.java
@@ -1,5 +1,13 @@
 package d.tonyandfriends.thirdtimesthecharm;
 
+import android.os.AsyncTask;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
@@ -355,7 +363,7 @@ public class ScannerStartActivity extends Activity implements DataTransporter, S
             apiButton.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
                     new ContactAPI().execute("http://18.216.191.20/php_rest_api/api/users/readAll.php");
-                    statusMessage.setText("blah");
+
                 }
             });
             reviewButton.setOnClickListener(new View.OnClickListener() {
@@ -698,6 +706,51 @@ public class ScannerStartActivity extends Activity implements DataTransporter, S
             super.onActivityResult(requestCode, resultCode, data);
         }
     }
+    public class ContactAPI extends AsyncTask<String, String, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line);
+                }
+                return buffer.toString();
+            }
+            catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
 
-
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            statusMessage.setText(result);
+        }
+    }
 }
+
