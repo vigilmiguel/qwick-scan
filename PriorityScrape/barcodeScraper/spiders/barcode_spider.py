4b5ffba393eac392b709df90dc036e2185656e05
eggoJJ <40075327+eggoJJ@users.noreply.github.com>
2019-12-05 13:15:42 -0800

    Dynamic Queue Crawler

---
+++ PriorityScrape/barcodeScraper/spiders/barcode_spider.py
@@ -0,0 +1,89 @@
+import scrapy
+import time
+from parseHelper import *
+
+
+
+# Run the python scrapes with the command : scrapy crawl barcodes
+class barcode_spider(scrapy.Spider):
+
+    name = 'barcodes'
+    secretCode = 'tDoYpc'
+    handle_httpstatus_list = [404]   
+    barHashTable = {}
+    fileNames = ['UBC-DB.html','Google-Shopping-Stop1.html','Review-Find-Stop1.html']
+    def start_requests(self):
+        barHashTable = {}
+        pages = {
+            'https://www.barcodelookup.com/', 
+            'https://www.google.com/search?psb=1&tbm=shop&q=',
+            'https://brickseek.com/products/?search='  
+        }
+        
+        yield scrapy.Request(url='https://www.barcodelookup.com/' + self.targetBarcode, callback=self.parse, meta={'index':0, 'barcode': self.targetBarcode, 'barHash': barHashTable})
+        #yield scrapy.Request(url='https://www.google.com/search?psb=1&tbm=shop&q=' + self.targetBarcode, callback=self.parse, meta={'index':1, 'barcode': self.targetBarcode, 'barHash': barHashTable})
+        yield scrapy.Request(url='https://brickseek.com/products/?search=' + self.targetBarcode, callback=self.parse, meta={'index':2, 'barcode': self.targetBarcode, 'barHash': barHashTable})
+
+
+    def parse(self, response):
+        
+        filename = 'poop-%d.html' % response.meta['index'] 
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+            self.log('Saved file %s' % filename)
+        if(response.meta['index'] == 0):
+           parseName(response.meta['barcode'],response.meta['barHash']) 
+           yield scrapy.Request(url='https://www.google.com/search?psb=1&tbm=shop&q=' + response.meta['barHash'].get(self.targetBarcode)[0], callback=self.parse0, meta={'index':1, 'barcode': self.targetBarcode, 'barHash': response.meta['barHash']})
+
+
+        '''
+        if(response.meta['index'] == 1):
+
+            temp = findURL()
+            if(temp != None):
+                yield scrapy.Request(url='https://www.google.com' + temp,callback=self.parse2, meta= {'barHash2': response.meta['barHash'], 'barcode2': response.meta['barcode']})
+            else:
+                cleanUP('PricePage.html')
+        ''' 
+        if(response.meta['index'] == 2):
+            temp = findURL2()
+            if(temp != None):
+                yield scrapy.Request(url ='https://brickseek.com'+ temp, callback=self.parse3 )
+            else:
+                cleanUP('ReviewPage.html')
+    
+
+
+    def parse0(self,response):
+
+        filename = 'poop-1.html'  
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+            self.log('Saved file %s' % filename)
+
+        temp = findURL()
+        if(temp != None):
+            yield scrapy.Request(url='https://www.google.com' + temp,callback=self.parse2, meta= {'barHash2': response.meta['barHash'], 'barcode2': response.meta['barcode']})
+        else:
+            cleanUP('PricePage.html')
+
+    def parse2(self,response):
+        filename = 'PricePage.html' 
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        getPrices(response.meta['barcode2'],response.meta['barHash2'])
+
+    def parse3(self,response):
+        filename = 'ReviewPage.html' 
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        getReviews(response.meta['barcode'])
+
+    def parse4(self,response):
+        filename = 'addressPage.html' 
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        getReviews(response.meta['barcode'])
+
+
+        
